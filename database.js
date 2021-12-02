a0b3916bcfe06ab333a23afec8c135779518635f
Andreas Wegner <andreaswegner@posteo.de>
2021-12-02 02:25:49 +0100

    Add database functions

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const mariadb = require('mariadb');
+const bcrypt = require('bcrypt');
 const Responses = require('./responses');
 
 const db_connect_pool = mariadb.createPool({
@@ -84,12 +85,41 @@ async function doQuery(query, values, errorHandler)
         {
             errorHandler.catch_callback(err);
         }
+        else
+        {
+            console.log("An error occured but no error handles was set in doQuery()");
+        }
     }
 }
 
 function getUsers(errorHandler)
 {
     return doQuery('SELECT * FROM customer LIMIT 200', [], errorHandler);
+}
+
+function getUsersWithProfiles(errorHandler)
+{
+    return doQuery(
+    `SELECT
+    customer.email,
+    customer.password,
+    customer.phone_number,
+    customer.first_name,
+    customer.last_name,
+    customer.registration_date,
+    customer.date_of_birth,
+    customer.deletion_id,
+    customer.profile_id,
+    customer.address_id,
+    customer.permission,
+    profile_customer.description,
+    profile_customer.last_online,
+    profile_customer.is_active,
+    profile_customer.latitude,
+    profile_customer.longtitude
+    FROM customer
+    LEFT JOIN profile_customer
+    ON customer.profile_id = profile_customer.profile_id`, [], errorHandler);
 }
 
 function getUserByEmail(email, errorHandler)
@@ -99,12 +129,66 @@ function getUserByEmail(email, errorHandler)
     errorHandler);
 }
 
+function getUserByProfileID(profile_id, errorHandler)
+{
+    return doQuery('SELECT * FROM customer WHERE profile_id = ? LIMIT 1',
+    [profile_id],
+    errorHandler);
+}
+
+async function getUserByProfileIDHome(profile_id, errorHandler)
+{
+    const rows = await doQuery(
+        `SELECT customer.email, customer.phone_number, customer.first_name, customer.last_name,
+        customer.registration_date, customer.date_of_birth,
+        profile_customer.description
+        FROM customer
+        INNER JOIN profile_customer
+        ON customer.profile_id = profile_customer.profile_id AND customer.profile_id = ?`,
+    [profile_id],
+    errorHandler);
+    return rows;
+}
+
+async function getUserByProfileIDGuest(profile_id, errorHandler)
+{
+    const rows = await doQuery(
+        `SELECT customer.first_name, customer.last_name,
+        customer.date_of_birth,
+        profile_customer.description, profile_customer.last_online
+        FROM customer
+        INNER JOIN profile_customer
+        ON customer.profile_id = profile_customer.profile_id AND customer.profile_id = ?`,
+    [profile_id],
+    errorHandler);
+    return rows;
+}
+
 async function isUserRegistered(email, errorHandler)
 {
     const rows = await doQuery('SELECT email FROM customer WHERE email = ? LIMIT 1',
     [email],
     errorHandler);
     return rows !== undefined && rows.length !== undefined && rows.length > 0;
+}
+
+async function authenticateUser(email, password, errorHandler)
+{
+    const rows = await doQuery('SELECT password FROM customer WHERE email = ? LIMIT 1',
+    [email],
+    errorHandler);
+    if(rows === undefined || rows.length === undefined || rows.length == 0)
+    {
+        return false;
+    }
+    try
+    {
+        return await bcrypt.compare(password, rows[0].password);
+    }
+    catch(err)
+    {
+        errorHandler.catch_callback(err);
+    }
 }
 
 function newUser(email, pw, phone, firstName, lastName, dob, profile_id, errorHandler)
@@ -126,8 +210,14 @@ async function newProfile(errorHandler)
 module.exports.getPool = getPool;
 
 module.exports.getUsers = getUsers;
+module.exports.getUsersWithProfiles = getUsersWithProfiles;
 module.exports.getUserByEmail = getUserByEmail;
+module.exports.getUserByProfileID = getUserByProfileID;
+module.exports.getUserByProfileIDHome = getUserByProfileIDHome;
+module.exports.getUserByProfileIDGuest = getUserByProfileIDGuest;
+
 module.exports.isUserRegistered = isUserRegistered;
+module.exports.authenticateUser = authenticateUser;
 module.exports.newUser = newUser;
 module.exports.newProfile = newProfile;
 
